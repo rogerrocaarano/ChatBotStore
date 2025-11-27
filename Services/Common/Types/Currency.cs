5de4e63aca3ab61c45017b3af8e37826ca4c2264
Roger Roca Arano <git@rogerroca.me>
2025-11-27 13:59:24 -0400

    feat(Common): add descriptions for currency enum values

--- Services/Common/Types/Currency.cs
+++ Services/Common/Types/Currency.cs
@@ -1,11 +1,18 @@
-using Common.Abstractions.Entities;
+using System.ComponentModel;
 
 namespace Common.Types;
 
 public enum Currency
 {
+    [Description("Bolivianos")]
     BOB,
+
+    [Description("Dólares Estadounidenses")]
     USD,
+
+    [Description("Libras Esterlinas")]
     GBP,
+
+    [Description("Euros")]
     EUR,
 }
