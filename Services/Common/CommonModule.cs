73491c83630deae6080961976989098fd13158d7
Roger Roca Arano <git@rogerroca.me>
2025-11-27 13:59:13 -0400

    feat(Common): add LiteBus Events and Queries support to CommonModule

--- Services/Common/CommonModule.cs
+++ Services/Common/CommonModule.cs
@@ -1,13 +1,18 @@
 using System.Reflection;
 using LiteBus.Commands;
+using LiteBus.Events;
 using LiteBus.Extensions.Microsoft.DependencyInjection;
+using LiteBus.Queries;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Common;
 
 public static class CommonModule
 {
-    public static IServiceCollection AddApplication(this IServiceCollection services, params Assembly[] assemblies)
+    public static IServiceCollection AddApplication(
+        this IServiceCollection services,
+        params Assembly[] assemblies
+    )
     {
         // Register LiteBus modules
         services.AddLiteBus(liteBus =>
@@ -15,6 +20,8 @@ public static class CommonModule
             foreach (var assembly in assemblies)
             {
                 liteBus.AddCommandModule(module => module.RegisterFromAssembly(assembly));
+                liteBus.AddEventModule(module => module.RegisterFromAssembly(assembly));
+                liteBus.AddQueryModule(module => module.RegisterFromAssembly(assembly));
             }
         });
 
